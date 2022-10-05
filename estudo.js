3de7fecf9c0a3c3ef5710c3a326f6f2f97ac69b5
isadora83 <110620611+isadora83@users.noreply.github.com>
2022-10-05 09:57:56 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,28 @@
+let tabuada = 2;
 
+function escreva(){
+    document.write(tabuada+"x 1 =" +(tabuada*1)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*2)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*3)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*4)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*5)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*6)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*7)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*8)+"<br>");
+    document.write(tabuada+"1="+(tabuada*9)+"<br>");
+    document.write(tabuada+"x 1 =" +(tabuada*10)+"<br>");
+}
+
+function minhatabuada(){
+    for(let i=1; i <=10; i++){
+        document.write("tabuada do"+ i +"<br>");
+        for(let j= 1 ; j<=10;++){
+            (document.write(i +" x "+j+ "=" +(j*i)+"<br>"));
+        }
+        document.write("<br>");
+    }
+]
+
+function cubo(){
+    for
+}
